d001d54a7727cb5bccdc21fdc232b1413deb43d1
Adrian Baddeley <adrian@maths.uwa.edu.au>
2003-11-12 18:02:22 +0000

    version 1.3-4

--- R/First.R
+++ R/First.R
@@ -1,8 +1,7 @@
 .First.lib <- function(lib, pkg) {
     library.dynam("spatstat", pkg, lib)
-    cat("spatstat 1.3-3\n")
+    cat("spatstat 1.3-4\n")
     cat("Type \"demo(spatstat)\" for a demonstration\n")
-#    locn <- paste(Sys.getenv("R_HOME"),"/library/spatstat/doc", sep="")
     locn <- paste(.path.package(package="spatstat"), "/doc", sep="")
     cat(paste("See the Introduction and Quick Reference in", locn, "\n"))
 }
