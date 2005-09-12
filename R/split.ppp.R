4796efbd463304a270ccd85c3ea78353df8ab76a
Adrian Baddeley <adrian@maths.uwa.edu.au>
2005-09-12 12:45:29 +0000

    version 1.6-12

--- R/split.ppp.R
+++ R/split.ppp.R
@@ -1,16 +1,14 @@
 #
 # split.R
 #
-# $Revision: 1.2 $ $Date: 2005/07/22 05:14:38 $
+# $Revision: 1.3 $ $Date: 2005/08/10 06:55:02 $
 #
 # split.ppp and "split<-.ppp"
 #
 #########################################
 
-split.ppp <- function(x, f = x$marks, drop=FALSE, un=NULL, ...) {
+split.ppp <- function(x, f = x$marks) {
   verifyclass(x, "ppp")
-  if(is.null(un))
-     un <- missing(f)
   if(!missing(f)) {
     if(!is.factor(f))
       stop("f must be a factor")
@@ -22,59 +20,42 @@ split.ppp <- function(x, f = x$marks, drop=FALSE, un=NULL, ...) {
     else
       stop("f is missing and there is no sensible default")
   }
-  lev <- levels(f)
-  if(drop) lev <- lev[table(f) > 0]
   out <- list()
-  for(l in lev) 
+  for(l in levels(f)) 
     out[[paste(l)]] <- x[f == l]
   
-  if(un)
-     out <- lapply(out, unmark)
   class(out) <- c("splitppp", class(out))
   return(out)
 }
 
-"split<-.ppp" <- function(x, f=x$marks, drop=FALSE, un=missing(f), 
-                          ..., value) {
+"split<-.ppp" <- function(x, f=x$marks, value) {
   verifyclass(x, "ppp")
-  stopifnot(is.list(value))
-  if(!all(unlist(lapply(value, is.ppp))))
-    stop("Each entry of \`value\' must be a point pattern")
-
+  fimplicit <- FALSE
   if(!missing(f)) {
     if(!is.factor(f))
       stop("f must be a factor")
     if(length(f) != x$n)
       stop("length(f) must equal the number of points in x")
   } else {
-    if(is.marked(x) && is.factor(x$marks))
-      f <- x$marks
-    else
+    if(!is.marked(x) || !is.factor(x$marks))
       stop("f is missing and there is no sensible default")
+    f <- x$marks
+    fimplicit <- TRUE
   }
-  if(!drop) {
-    lev <- levels(f)
-    levtype <- "levels of f"
-  } else {
-    lev <- levels(f)[table(f) > 0]
-    levtype <- "levels which f actually takes"
-  }
-  if(length(value) != length(lev))
-      stop(paste("length of \`value\' should equal the number of",
-                 levtype))
-  
-  if(is.null(names(value)))
-    names(value) <- paste(lev)
-  else if(!all(names(value) %in% paste(levels(f))))
-    stop("names of \`value\' should be levels of f")
+  if(!is.list(value) || length(value) != length(levels(f)))
+    stop("value must be a list, with an entry for each level of f")
+  if(!all(unlist(lapply(value, is.ppp))))
+    stop("Each entry of \`value\' must be a point pattern")
 
-  out <- x
-  for(l in lev) {
-    v <- value[[paste(l)]]
-    if(un)
-      v <- v %mark% factor(l, levels=levels(f))
-    out[f == l] <- v
+  if(is.null(names(value))) {
+    if(fimplicit)
+      names(value) <- paste(levels(f)) # these become the marks
+  }else {
+    if(!all(names(value) %in% paste(levels(f))))
+      stop("names of \`value\' should be levels of f")
   }
+
+  out <- superimpose(value)
 
   return(out)
 }
