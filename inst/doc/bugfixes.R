c79c584b197e7312cf4abcce1f207a3529136f8b
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2022-04-01 07:30:02 +0000

    version 2.3-4

--- inst/doc/bugfixes.R
+++ inst/doc/bugfixes.R
@@ -1,16 +1,18 @@
 ### R code from vignette source 'bugfixes.Rnw'
 
 ###################################################
-### code chunk number 1: bugfixes.Rnw:20-24
+### code chunk number 1: bugfixes.Rnw:20-26
 ###################################################
 library(spatstat)
-sversion <- read.dcf(file = system.file("DESCRIPTION", package = "spatstat"),
-         fields = "Version")
+x <- read.dcf(file = system.file("DESCRIPTION", package = "spatstat"),
+              fields = c("Version", "Date"))
+sversion <- as.character(x[,"Version"])
+sdate    <- as.character(x[,"Date"])
 options(useFancyQuotes=FALSE)
 
 
 ###################################################
-### code chunk number 2: bugfixes.Rnw:38-42
+### code chunk number 2: bugfixes.Rnw:40-44
 ###################################################
 nbugs <- nrow(news(grepl("^BUG", Category), 
                    package="spatstat"))
@@ -19,32 +21,60 @@ nbugssince <- nrow(news(Version > "1.42-0" & grepl("^BUG", Category),
 
 
 ###################################################
-### code chunk number 3: bugfixes.Rnw:58-59 (eval = FALSE)
+### code chunk number 3: bugfixes.Rnw:60-61 (eval = FALSE)
 ###################################################
 ## bugfixes
 
 
 ###################################################
-### code chunk number 4: bugfixes.Rnw:63-64 (eval = FALSE)
+### code chunk number 4: bugfixes.Rnw:65-66 (eval = FALSE)
 ###################################################
 ## bugfixes(sinceversion="1.50-0")
 
 
 ###################################################
-### code chunk number 5: bugfixes.Rnw:68-69 (eval = FALSE)
+### code chunk number 5: bugfixes.Rnw:70-71 (eval = FALSE)
 ###################################################
 ## bugfixes(sincedate="2017-06-30")
 
 
 ###################################################
-### code chunk number 6: bugfixes.Rnw:72-73 (eval = FALSE)
+### code chunk number 6: bugfixes.Rnw:74-75 (eval = FALSE)
 ###################################################
 ## bugfixes("book")
 
 
 ###################################################
-### code chunk number 7: bugfixes.Rnw:76-77 (eval = FALSE)
+### code chunk number 7: bugfixes.Rnw:78-79 (eval = FALSE)
 ###################################################
 ## bugfixes("all")
+
+
+###################################################
+### code chunk number 8: bugfixes.Rnw:89-106
+###################################################
+getstuff <- function(pkg) {
+  x <- read.dcf(file=system.file("DESCRIPTION", package=pkg),
+                fields=c("Version", "Date"))
+  xversion <- as.character(x[,"Version"])
+  xdate    <- as.character(x[,"Date"])
+  data.frame(date=as.Date(xdate), package=pkg, version=xversion)
+}
+vtable <- do.call(rbind,
+                  lapply(c("spatstat.utils",
+                           "spatstat.data", 
+                           "spatstat.sparse",
+                           "spatstat.geom",
+                           "spatstat.random",
+                           "spatstat.core",
+                           "spatstat.linnet",
+                           "spatstat"),
+                         getstuff))
+
+
+###################################################
+### code chunk number 9: bugfixes.Rnw:112-113
+###################################################
+print(vtable, row.names=FALSE)
 
 
